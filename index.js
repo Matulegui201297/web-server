4fc17ad185f541d29ee553fd02dac1c710c32ce0
Mateo Leguizamon <matu_legui_22@hotmail.com>
2022-01-20 06:13:24 -0300

    Primer commit del web server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require ('express');
 const app = express();
-const port = 3000
+const port = process.env.port || 3000 ;
+
 
 app.use(express.static(__dirname+ '/public'));
 
